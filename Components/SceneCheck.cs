aab69334526d1e47c57bc5423b07c96a69f493e5
j_crakers <john@jutras.net>
2025-05-31 19:06:57 -0400

    SceneCheck

---
+++ Components/SceneCheck.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UltrakillArtemisMod.Components;
+
+public static class SceneCheck
+{
+    private static bool initialized;
+    public static void Init()
+    {
+        if (!initialized)
+        {
+            initialized = true;
+            SceneManager.sceneLoaded += OnSceneLoad;
+        }
+    }
+
+    //Code frome EasyPZ which is made by Hydraxous
+    //https://github.com/Hydraxous/EasyPZ-ULTRAKILL
+    //Modified by me
+
+    /// <summary>
+    /// Enumerated version of the Ultrakill scene types
+    /// </summary>
+    public enum LevelType { Intro, MainMenu, Tutorial, Level, Credits, Sandbox, CyberGrind, Secret, PrimeSanctum, Encore, Custom, Unknown }
+    /// <summary>
+    /// Returns the current level type
+    /// </summary>
+    public static LevelType CurrentLevelType = LevelType.Intro;
+
+    /// <summary>
+    /// Returns the currently active ultrakill scene name.
+    /// </summary>
+    public static string CurrentSceneName = "";
+
+    public delegate void OnLevelChangedHandler(LevelType LevelType);
+
+    /// <summary>
+    /// Invoked whenever the current level type is changed.
+    /// </summary>
+    public static OnLevelChangedHandler OnLevelTypeChanged;
+
+    /// <summary>
+    /// Invoked whenever the scene is changed.
+    /// </summary>
+    public static OnLevelChangedHandler OnLevelChanged;
+
+
+    private static void OnSceneLoad(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene != SceneManager.GetActiveScene())
+            return;
+
+        LevelType newScene = GetLevelType(SceneHelper.CurrentScene);
+
+        if (newScene != CurrentLevelType)
+        {
+            CurrentLevelType = newScene;
+            CurrentSceneName = SceneHelper.CurrentScene;
+            OnLevelTypeChanged?.Invoke(newScene);
+        }
+
+
+        OnLevelChanged?.Invoke(CurrentLevelType);
+    }
+    
+    public static LevelType GetLevelType(string sceneName)
+    {
+        sceneName = sceneName.Contains("P-") ? "PrimeSanctum" : sceneName;
+        sceneName = sceneName.Contains("-S") ? "Secret" : sceneName;
+        sceneName = sceneName.Contains("Level") ? "Level" : sceneName;
+        sceneName = sceneName.Contains("-E") ? "Encore" : sceneName;
+
+        return sceneName switch
+        {
+            "Intro" => LevelType.Intro,
+            "Main Menu" => LevelType.MainMenu,
+            "Tutorial" => LevelType.Tutorial,
+            "Level" => LevelType.Level,
+            "CreditsMuseum2" => LevelType.Credits,
+            "uk_construct" => LevelType.Sandbox,
+            "Endless" => LevelType.CyberGrind,
+            "Secret" => LevelType.Secret,
+            "PrimeSanctum" => LevelType.PrimeSanctum,
+            "Custom" => LevelType.Custom,
+            _ => LevelType.Unknown,
+        };
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using GameConsole.pcon;
+using System.Net.Http;
 using ULTRAKILL;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UltrakillArtemisMod.SceneCheck;
 
 namespace UltrakillArtemisMod;
 
@@ -12,19 +14,25 @@ public class ArtemisSupport : BaseUnityPlugin
 {
     public static float seconds;
     public static new ManualLogSource Logger;
-    //private NewMovement player;
+    //static readonly HttpClient client = new HttpClient();
+
+    private GetArtemisWebServerPort()
+    {
+        
+    }
 
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
 
-        InGameCheck.Init();
 
-        InGameCheck.OnLevelChanged += OnLevelChanged;
+
+        SceneCheck.Init();
+        SceneCheck.OnLevelChanged += OnLevelChanged;
+        SceneCheck.OnLevelTypeChanged += OnLevelTypeChanged;
 
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        Logger.LogInfo($"Current level type: {InGameCheck.CurrentLevelType}");
     }
     
     private void Update()
@@ -36,92 +44,13 @@ public class ArtemisSupport : BaseUnityPlugin
         }
     }
 
-    
-
-    private static void OnLevelChanged(InGameCheck.LevelType levelType)
+    private void OnLevelChanged(LevelType levelType)
     {
-        Logger.LogInfo($"Scene changed to {InGameCheck.CurrentSceneName}");
-        Logger.LogInfo($"Level changed to {levelType}");
-    }
-}
-
-public static class InGameCheck
-{
-    private static bool initialized;
-    public static void Init()
-    {
-        if (!initialized)
-        {
-            initialized = true;
-            SceneManager.sceneLoaded += OnSceneLoad;
-        }
-    }
-
-    /// <summary>
-    /// Enumerated version of the Ultrakill scene types
-    /// </summary>
-    public enum LevelType { Intro, MainMenu, Tutorial, Level, Credits, Sandbox, CyberGrind, Secret, PrimeSanctum, Encore, Custom, Unknown }
-    /// <summary>
-    /// Returns the current level type
-    /// </summary>
-    public static LevelType CurrentLevelType = LevelType.Intro;
-
-    /// <summary>
-    /// Returns the currently active ultrakill scene name.
-    /// </summary>
-    public static string CurrentSceneName = "";
-
-    public delegate void OnLevelChangedHandler(LevelType LevelType);
-
-    /// <summary>
-    /// Invoked whenever the current level type is changed.
-    /// </summary>
-    public static OnLevelChangedHandler OnLevelTypeChanged;
-
-    /// <summary>
-    /// Invoked whenever the scene is changed.
-    /// </summary>
-    public static OnLevelChangedHandler OnLevelChanged;
-
-
-    private static void OnSceneLoad(Scene scene, LoadSceneMode loadSceneMode)
-    {
-        if (scene != SceneManager.GetActiveScene())
-            return;
-
-        LevelType newScene = GetLevelType(SceneHelper.CurrentScene);
-
-        if (newScene != CurrentLevelType)
-        {
-            CurrentLevelType = newScene;
-            CurrentSceneName = SceneHelper.CurrentScene;
-            OnLevelTypeChanged?.Invoke(newScene);
-        }
-
-
-        OnLevelChanged?.Invoke(CurrentLevelType);
+        Logger.LogInfo($"Level changed to {SceneCheck.CurrentSceneName}");
     }
     
-    public static LevelType GetLevelType(string sceneName)
+    private void OnLevelTypeChanged(LevelType levelType)
     {
-        sceneName = sceneName.Contains("P-") ? "PrimeSanctum" : sceneName;
-        sceneName = sceneName.Contains("-S") ? "Secret" : sceneName;
-        sceneName = sceneName.Contains("Level") ? "Level" : sceneName;
-        sceneName = sceneName.Contains("-E") ? "Encore" : sceneName;
-
-        return sceneName switch
-        {
-            "Intro" => LevelType.Intro,
-            "Main Menu" => LevelType.MainMenu,
-            "Tutorial" => LevelType.Tutorial,
-            "Level" => LevelType.Level,
-            "CreditsMuseum2" => LevelType.Credits,
-            "uk_construct" => LevelType.Sandbox,
-            "Endless" => LevelType.CyberGrind,
-            "Secret" => LevelType.Secret,
-            "PrimeSanctum" => LevelType.PrimeSanctum,
-            "Custom" => LevelType.Custom,
-            _ => LevelType.Unknown,
-        };
+        Logger.LogInfo($"Level type changed to {levelType}");
     }
 }
